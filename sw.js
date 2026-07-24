b6f723183a74fcb14f61c049fe9bb142e0cd6318
Isabirye-alex <alexisabirye558@gmail.com>
2026-07-24 22:38:11 +0300

    feat: add Progressive Web App (PWA) manifest and Service Worker caching

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'sacco-member-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './css/style.css',
+  './js/main.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
